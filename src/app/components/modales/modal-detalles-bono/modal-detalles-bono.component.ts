3882e5038e27582486c9cb37ffa0a6a1426e37b2
Diego David Rutti Aliaga <drutti@sunarp.gob.pe>
2025-07-01 15:07:30 -0500

    modal detalles v2

--- src/app/components/modales/modal-detalles-bono/modal-detalles-bono.component.ts
+++ src/app/components/modales/modal-detalles-bono/modal-detalles-bono.component.ts
@@ -1,28 +1,66 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
 
 @Component({
   selector: 'app-modal-detalles-bono',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NzIconModule],
   templateUrl: './modal-detalles-bono.component.html',
   styleUrls: ['./modal-detalles-bono.component.scss']
 })
 export class ModalDetallesBonoComponent {
 
-  
+
   @Input() bono: any;
   @Input() visible: boolean = false;
   @Output() cerrar = new EventEmitter<void>();
   @Output() abrirPagos = new EventEmitter<void>();
 
-  
+  estadoFavorito: boolean = false;
+  estadoMeGusta: boolean = false;
+  estadoVista: boolean = false;
+
+  animFavorito: string = '';
+  animMeGusta: string = '';
+  animVista: string = '';
+
+  toggleFavorito() {
+    if (this.estadoFavorito) {
+      this.animFavorito = 'desactivando';
+    } else {
+      this.animFavorito = 'activando';
+    }
+    this.estadoFavorito = !this.estadoFavorito;
+    setTimeout(() => this.animFavorito = '', 400);
+  }
+
+  toggleMeGusta() {
+    if (this.estadoMeGusta) {
+      this.animMeGusta = 'desactivando';
+    } else {
+      this.animMeGusta = 'activando';
+    }
+    this.estadoMeGusta = !this.estadoMeGusta;
+    setTimeout(() => this.animMeGusta = '', 400);
+  }
+
+  toggleVista() {
+    if (this.estadoVista) {
+      this.animVista = 'desactivando';
+    } else {
+      this.animVista = 'activando';
+    }
+    this.estadoVista = !this.estadoVista;
+    setTimeout(() => this.animVista = '', 400);
+  }
 
   cerrarModal() {
     this.cerrar.emit();
   }
 
-  
+
 
   abrirModalPagos() {
     this.abrirPagos.emit();
